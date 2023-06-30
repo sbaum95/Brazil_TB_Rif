a464934bc65745353467d61699a1ad878eb0bc89
sbaum95 <sbaum@g.harvard.edu>
2023-06-29 17:08:18 -0700

    cleaned up and added in health unit script

--- code/00_load_sinan.R
+++ code/00_load_sinan.R
@@ -7,17 +7,6 @@ source(here::here("code/dependencies.R"))
 
 # load data ---------------------------------------------------------------
 sinan_xpert <- read_dta("data/fim_abr_2023_no name.dta") %>% 
-  # select(dt_notific, nu_ano, sg_uf_not, id_municip, id_regiona, id_unidade, 
-  #        dt_diag, dt_nasc, nu_idade_n, cs_sexo, cs_raca, sg_uf, id_mn_resi, 
-  #        tratamento, raiox_tora, forma, agravaids, hiv, agravalcoo, agravdiabe,
-  #        bacilosc_e, cultura_es, histopatol, dt_inic_tr, rifampicin, isoniazida, 
-  #        estreptomi, etionamida, outras, sg_uf_at, id_munic_a, bacilosc_1, 
-  #        bacilosc_2, bacilosc_3, bacilosc_4, bacilosc_5, bacilosc_6, situa_ence, 
-  #        dt_encerra, test_molec
-  #        # test_sensi, 
-  #        # bac_apos_6, 
-  #        # transf
-  # ) %>% 
   mutate_at(c("nu_ano", "sg_uf_not", "id_municip", "id_regiona", "id_unidade", "nu_idade_n", "sg_uf", "id_mn_resi"), as.numeric) %>% 
   mutate_at(c("dt_notific", "dt_diag", "dt_encerra"), as.Date, format = "%Y-%m-%d") %>% 
   mutate(age = floor(as.numeric((dt_diag - dt_nasc)/365))) %>% # create age variable 
@@ -27,8 +16,11 @@ sinan_xpert <- read_dta("data/fim_abr_2023_no name.dta") %>%
          diag_yr = floor_date(as_date(dt_diag), "year"),
          init_yr = floor_date(as_date(dt_inic_tr), "year"),
          init_yrmo = floor_date(as_date(dt_inic_tr), "month"),
-         close_yrmo = floor_date(as_date(dt_encerra), "month")) %>% 
-  filter(diag_yr >= "2014-01-01")
+         close_yrmo = floor_date(as_date(dt_encerra), "month")) 
+# %>% filter(diag_yr >= "2014-01-01")
+
+
+
 
 # create age cat 
 sinan_xpert$age_cat <- case_when(sinan_xpert$age > 0 & sinan_xpert$age < 5 ~ "0-4",
@@ -60,58 +52,41 @@ sinan_xpert$situa_ence <- case_when(sinan_xpert$situa_ence %in% c(" 1", "01", "1
 sinan_xpert$situa_ence <- factor(sinan_xpert$situa_ence)
 
 
-# create age cat 
-sinan_xpert$age_cat <- case_when(sinan_xpert$age > 0 & sinan_xpert$age < 5 ~ "0-4",
-                                 sinan_xpert$age >= 5 & sinan_xpert$age < 15 ~ "5-14",
-                                 sinan_xpert$age >= 15 & sinan_xpert$age < 25 ~ "15-24",
-                                 sinan_xpert$age >= 25 & sinan_xpert$age < 35 ~ "25-34",
-                                 sinan_xpert$age >= 35 & sinan_xpert$age < 45 ~ "35-44",
-                                 sinan_xpert$age >= 45 & sinan_xpert$age < 55 ~ "45-54",
-                                 sinan_xpert$age >= 55 & sinan_xpert$age < 65 ~ "55-64", 
-                                 sinan_xpert$age >= 65 ~ "65+")
-
-sinan_xpert$age_cat <- factor(sinan_xpert$age_cat)
 
 # add state names
 state_name <- read_xlsx("data/StateCodes.xlsx")
-
-sinan_xpert <- left_join(sinan_xpert, state_name %>% 
-                        select(sg_uf, NAME_1, NAME_2), by = "sg_uf") %>% 
-  rename(state_nm = NAME_1, region_nm = NAME_2)
+sinan_xpert <- left_join(sinan_xpert, state_name %>% select(sg_uf, NAME_1, NAME_2), by = "sg_uf") %>% rename(state_nm = NAME_1, region_nm = NAME_2)
 
 
 
 
-# add population data 
+# add population data & municipality names
 population <- read_excel("data/MunicipalCodes.xlsx") %>% 
   select(cod.6.2010, cod.sg.2010, pop.tot.2010, "Município 2010") %>% 
   rename(id_municip = cod.6.2010, id_state = cod.sg.2010, municip_name = "Município 2010", pop_total_2010 = pop.tot.2010) %>% 
   mutate(id_municip = as.numeric(id_municip), 
          pop_total_2010 = as.numeric(gsub("\\.", "", pop_total_2010)),
-         id_state = as.numeric(id_state)) %>% 
-  group_by(id_state) %>% 
-  summarize(state_pop = sum(pop_total_2010))
+         id_state = as.numeric(id_state)) 
 
-sinan_xpert <- left_join(sinan_xpert, population, by = c("sg_uf" = "id_state"))
+## state population data
+sinan_xpert <- left_join(sinan_xpert, population %>% group_by(id_state) %>% summarize(state_pop = sum(pop_total_2010)), by = c("sg_uf" = "id_state"))
 
-# add municipality names 
-population <- read_excel("data/MunicipalCodes.xlsx") %>% 
-  select(cod.6.2010, cod.sg.2010, pop.tot.2010, "Município 2010") %>% 
-  rename(id_municip = cod.6.2010, id_state = cod.sg.2010, municip_name = "Município 2010", pop_total_2010 = pop.tot.2010) %>% 
-  mutate(id_municip = as.numeric(id_municip), 
-         pop_total_2010 = as.numeric(gsub("\\.", "", pop_total_2010)),
-         id_state = as.numeric(id_state)) %>% 
-  select(id_municip, municip_name)
-
-sinan_xpert <- left_join(sinan_xpert, population, by = "id_municip") 
-# sinan_xpert$id_municip <- as.factor(sinan_xpert$id_municip)
+## municipality name
+sinan_xpert <- left_join(sinan_xpert, population %>% select(id_municip, municip_name), by = "id_municip") 
 
 
-# add health unit type data (Source: Do)
-source(here::here("code/pull_health_unit.R"))
 
 
+
+# add health unit type data (Source: Do) 
+source(here::here("code/00_add_health_unit_type.R"))
+
+sinan_xpert <- merged_health_unit %>% select(!c("state", "municip_cod", "cnes", "uni_tp_cod", "uni_tp", "year", "first_level", "second_level_tb", "third_level_tb", "second_level", "third_level", "others"))
+
+colnames(sinan_xpert) <- tolower(colnames(sinan_xpert))
 
 
 # write file --------------------------------------------------------------
 save(sinan_xpert, file = "data/sinan_xpert.Rdata")
+
+rm(population, state_name, merged_health_unit)
--- code/models.R
+++ code/models.R
@@ -1,6 +1,7 @@
 # Author: Sarah Baum
 # Created: 2023-06-26
-# Updated: 2023-06-27
+# Updated: 2023-06-29
+
 # Description: 
 # - Run Model 1: Probability of receiving Xpert, conditional on covariates
 # - Run Model 2: Probability of RR positive outcome, conditional on covariates 
@@ -10,18 +11,25 @@ source(here::here("code/dependencies.R"))
 
 load("data/sinan_xpert.Rdata")
 
+sinan_xpert <- sinan_xpert %>% filter(diag_yr >= "2014-01-01" & diag_yr < "2022-01-01")
+
+
 ## Model 1: Probability of receiving Xpert, conditional on covariates
 
+# Create outcome variable: 
+# test_molec responses are sensitive, resistant, not detectable, or inconclusive
 
-
-# Create outcome variable: test_molec is sensitive, resistant, not detectable, or inconclusive
 tabyl(sinan_xpert, test_molec, diag_yr) # Most missing are from 2014 and 2015 
-sinan_xpert$receive_xpert <- if_else(sinan_xpert$test_molec %in% c("1", "2", "3", "4"), 1, 
-                                     if_else(sinan_xpert$test_molec %in% c("5"), 0, NA))
-levels(sinan_xpert$test_molec)
 
-# Covariates: Age, Previous diagnosis with TB, HIV status, Type of Facility, Time, and Geography (hard with municipalities and data)
+sinan_xpert$receive_xpert <- if_else(sinan_xpert$test_molec %in% c("1", "2", "3", "4"), 1, 0) 
+
+
+
+
+# Covariates: Age, Previous diagnosis with TB, HIV status, Type of Facility, and year
 ## Age: Defined as difference between diagnosis and birth date
+
+
 
 ## Previous TB history:  Previous TB diagnosis = 1 if tratamento == 2 or 3; 0 if new case 
 sinan_xpert$tb_hist <- if_else(sinan_xpert$tratamento %in% c("2", "3"), 1, 
@@ -29,27 +37,56 @@ sinan_xpert$tb_hist <- if_else(sinan_xpert$tratamento %in% c("2", "3"), 1,
                                        if_else(sinan_xpert$tratamento %in% c("5"), 2, 
                                                if_else(sinan_xpert$tratamento %in% c("6"), 3, NA))))
 tabyl(sinan_xpert, tratamento)                              
-    ### I'm not sure if I need to be concerned about post-mortems 
-    ### Remove if unknown 
-    ### What to do about transfers?
+
+
+
 
 ## HIV Status: There are two HIV/AIDS variables, agravaids and hiv. I think agravaids is asked before you would know the hiv test result. 
-tabyl(sinan_xpert, agravaids, hiv)
+tabyl(sinan_xpert, agravaids)
 
-sinan_xpert$agravaids <- if_else(sinan_xpert$agravaids == 1, 1, 
-                                 if_else(sinan_xpert$agravaids == 2, 0, NA))
+sinan_xpert$agravaids <- if_else(sinan_xpert$agravaids == 1, 1, # 1 - HIV positive
+                                 if_else(sinan_xpert$agravaids == 2, 0, NA)) # 0 - HIV negative
 
 
-# run model 
-prop_mod <- glm(receive_xpert ~ age + I(age^2) + factor(tb_hist) + factor(agravaids) + factor(cs_sexo) + factor(cs_zona), 
+## Sex: 
+sinan_xpert$sex <- if_else(sinan_xpert$cs_sexo == "M", 1, 
+                           if_else(sinan_xpert$cs_sexo == "F", 0, NA))
+
+tabyl(sinan_xpert, nu_idade_n)
+
+# Model A - All years
+pr_mod_all <- glm(receive_xpert ~ age + factor(tratamento), 
                 family = "binomial" (link = "logit"), 
                 data = sinan_xpert)
-## Add cs_zona in 
 
 
-summary(prop_mod)
+summary(pr_mod_all)
 
-exp(coef(prop_mod))
-exp(confint(prop_mod))
+exp(coef(pr_mod_all))
+exp(confint(pr_mod_all))
 
+sinan_xpert$pr_score <- predict(pr_mod_all, type = "response")
+
+treated <- sinan_xpert %>% 
+  filter(treat == 1) %>%
+  ggplot(., aes(pr_score)) +
+  geom_histogram() + 
+  labs (x = "Propensity Score", 
+        y = "Count") + 
+  theme_bw() + 
+  ggtitle("Treated")
+
+# 2 - untreated units
+untreated <-lalonde_psid %>% 
+  filter(pr_score > 0.0000157, treat == 0) %>%
+  ggplot(., aes(pr_score)) +
+  geom_histogram() + 
+  labs (x = "Propensity Score", 
+        y = "Count") + 
+  theme_bw() + 
+  ggtitle("Untreated")
+
+
+# Model: By year 
+pr
 
