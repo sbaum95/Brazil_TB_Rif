3197712d521496645c163c009ef4df7717721f3a
sbaum95 <sbaum@g.harvard.edu>
2024-01-24 16:43:51 -0500

    Added model 3 (2017-2019) models; remove interaction term script

--- code/time_trend_model/municip-level/tt_gam_models-mun.R
+++ code/time_trend_model/municip-level/tt_gam_models-mun.R
@@ -30,14 +30,14 @@ load(here::here("data/mdf_new_ind.Rdata"))
 ##                     Model 1 - 2014-2019                     ##
 #################################################################
 
-mun_new_mod1b <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 23) + s(time, by = state, id = 1, k = 23) + age_cat + hiv_status + sex + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+mun_new_mod1 <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 23) + s(time, by = state, id = 1, k = 23) + age_cat + hiv_status + sex + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
                     data = mdf_mun_new_grp,
                     family = binomial (link = "logit"),
                     method = "REML")
 
 
 
-save(mun_new_mod1b, file = "output/fits/mun_new_mod1b.rda")
+save(mun_new_mod1, file = "output/fits/mun_new_mod1.rda")
 
 summary(mun_new_mod1)
 appraise(mun_new_mod1)
@@ -48,33 +48,28 @@ plot(mun_new_mod1)
 #################################################################
 ##                     Model 2 - 2016-2019                     ##
 #################################################################
-mun_new_mod2b <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 15) + s(time, by = state, id = 1, k = 15) + age_cat + hiv_status + sex + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
-                data = mdf_mun_new_grp %>% filter(time > 8),
+mun_new_mod2 <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 12) + s(time, by = state, id = 1, k = 12) + age_cat + hiv_status + sex + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+                        data = mdf_mun_new_grp %>% filter(time > 12),
+                        # %>% filter(time > 8),
+                        family = binomial (link = "logit"),
+                        method = "REML")
+
+save(mun_new_mod2, file = "output/fits/mun_new_mod2.rda")
+
+
+
+
+#################################################################
+##                     Model 3 - 2017-2019                     ##
+#################################################################
+mun_new_mod3 <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 12) + s(time, by = state, id = 1, k = 12) + age_cat + hiv_status + sex + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+                data = mdf_mun_new_grp %>% filter(time > 12),
                 family = binomial (link = "logit"),
                 method = "REML")
 
 
+save(mun_new_mod3, file = "output/fits/mun_new_mod3.rda")
 
-save(mun_new_mod2b, file = "output/fits/mun_new_mod2b.rda")
-
-summary(mun_new_mod2)
-appraise(mun_new_mod2)
-gam.check(mun_new_mod2)
-plot(mun_new_mod2)
-
-
-
-## Model 3 - All covs + interaction term -----------------------------------
-# mun_new_mod3 <- gam(cbind(Positive,Negative) ~ s(state, bs = "re") + s(time, k = 23) + s(time, by = state, id = 1, k = 23) + age_cat + hiv_status + sex + (age_cat*hiv_status*sex) + health_unit + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
-#                 data = mdf_mun_new_grp,
-#                 family = binomial (link = "logit"),
-#                 method = "REML")
-# 
-# save(mun_new_mod3, file = "output/fits/mun_new_mod3.rda")
-# 
-# summary(mun_new_mod3)
-# gam.check(mun_new_mod3)
-# plot(mun_new_mod3)
 
 
 
@@ -92,24 +87,20 @@ plot(mun_new_mod2)
 # Combines patient who are identified as relapsed and re-treatment
 load(here::here("data/mdf_prev_ind.Rdata"))
 
-# mdf_prev_ind <- mdf_prev_ind %>% 
-# filter out 0-4 year olds from model 
-
-
 
 
 #################################################################
 ##                     Model 1 - 2014-2019                     ##
 #################################################################
-mun_prev_mod1a  <- gam(result ~ s(state, bs = "re") + s(time, k = 23) + s(time, by = state, id = 1, k = 23) + age_cat + hiv_status + sex + health_unit + tratamento + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+mun_prev_mod1  <- gam(result ~ s(state, bs = "re") + s(time, k = 23) + s(time, by = state, id = 1, k = 23) + age_cat + hiv_status + sex + health_unit + tratamento + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
                       data = mdf_prev_ind %>% filter(!age_cat == "0-4"),
                       family = binomial (link = "logit"),
                       method = "REML")
 
 
-save(mun_prev_mod1a, file = "output/fits/mun_prev_mod1a.Rda")
+save(mun_prev_mod1, file = "output/fits/mun_prev_mod1.Rda")
 
-summary(mun_prev_mod1a)
+summary(mun_prev_mod1)
 appraise(mun_prev_mod1)
 gam.check(mun_prev_mod1)
 plot(mun_prev_mod1a)
@@ -120,10 +111,23 @@ plot(mun_prev_mod1a)
 #################################################################
 ##                     Model 2 - 2016-2019                     ##
 #################################################################
-mun_prev_mod2a <- gam(result~ s(state, bs = "re") + s(time, k = 15) + s(time, by = state, id = 1, k = 15) + hiv_status + age_cat + sex + health_unit + tratamento + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+mun_prev_mod2 <- gam(result~ s(state, bs = "re") + s(time, k = 15) + s(time, by = state, id = 1, k = 15) + hiv_status + age_cat + sex + health_unit + tratamento + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
                                  data = mdf_prev_ind %>% filter(time > 8) %>% filter(!age_cat == "0-4"),
                                  family = binomial (link = "logit"),
                                  method = "REML")
 
-save(mun_prev_mod2a, file = "output/fits/mun_prev_mod2a.Rda")
-plot(mun_prev_mod2a)
+save(mun_prev_mod2, file = "output/fits/mun_prev_mod2.Rda")
+
+
+
+
+
+#################################################################
+##                     Model 3 - 2017-2019                     ##
+#################################################################
+mun_prev_mod3 <- gam(result~ s(state, bs = "re") + s(time, k = 12) + s(time, by = state, id = 1, k = 12) + hiv_status + age_cat + sex + health_unit + tratamento + mun_has_prison + mun_urban_cat + mun_bf_cat + mun_fhs_cat,
+                     data = mdf_prev_ind %>% filter(time > 12) %>% filter(!age_cat == "0-4"),
+                     family = binomial (link = "logit"),
+                     method = "REML")
+
+save(mun_prev_mod3, file = "output/fits/mun_prev_mod3.Rda")
