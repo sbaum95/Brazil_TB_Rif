1a71f034824de45d4b6dcb5805a9d951b8727a20
sbaum95 <sbaum@g.harvard.edu>
2024-03-25 17:06:29 -0400

    added time stamps

--- code/02_run_models/05_run_all.R
+++ code/02_run_models/05_run_all.R
@@ -1,22 +1,23 @@
 # Author: Sarah Baum
 # Created: 2024-03-23
-# Updated:
+# Updated: 2024-03-25
 
 # Description: Runs models, gets fitted values and uncertainity results, and compiles results 
 
 source("code/dependencies.R")
 
+## drop sex from create_an dataset
 tictoc::tic()
-source("code/02_run_models/01_run_models.R")
+source("code/02_run_models/01_run_models.R") # ~ 5 hours for all models
 tictoc::toc()
 
 tictoc::tic()
-source("code/02_run_models/02_get_fitted_values.R")
+source("code/02_run_models/02_get_fitted_values.R") # 8 minutes
 tictoc::toc()
 
 # Not sure why this isn't running through lapply?
 tictoc::tic()
-source("code/02_run_models/03_get_uncertainity_intervals.R")
+source("code/02_run_models/03_get_uncertainity_intervals.R") # 1.5 hours
 tictoc::toc()
 
 tictoc::tic()
