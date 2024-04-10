41fd188bf804c4410dd1191caea463add7292c16
sbaum95 <sbaum@g.harvard.edu>
2024-04-09 22:42:15 -0400

    Changed from per 1,000 to percent to match WHO

--- code/03_make_figures_and_tables/fig_model_output_and_testing_trends.R
+++ code/03_make_figures_and_tables/fig_model_output_and_testing_trends.R
@@ -40,12 +40,10 @@ plot_observed <- function(quarter, case_type1 = NULL, case_type2 = NULL) {
     ggplot() + 
     
     # Observed incidence
-    geom_point(aes(x = diag_qrt, y = (obs_RR/obs_num_tested)*1000, color = case_type, size = obs_num_tested), alpha = 0.5) + 
+    geom_point(aes(x = diag_qrt, y = (obs_RR/obs_num_tested)*100, color = case_type, size = obs_num_tested), alpha = 0.5) + 
     
     # Percent tested among observed
-    geom_line(aes(x = diag_qrt, y = (obs_pct_tested*100)*(300/40), color = case_type), alpha = 0.5)  
-  
-    
+    geom_line(aes(x = diag_qrt, y = (obs_pct_tested*100), color = case_type), alpha = 0.5)  
 
 }
 
@@ -57,7 +55,8 @@ plot_model <- function(plot, agg_level, model_name, case_type){
   geom_line(
     data = compiled_results[[agg_level]] %>% 
       filter(model == model_name & case_type == case_type),
-    aes(x = diag_qrt, y = (fitted_RR/total_TB_cases)*1000, color = case_type, linetype = model))
+    aes(x = diag_qrt, y = (fitted_RR/total_TB_cases)*100, color = case_type, linetype = model))
+    
   
 }
 
@@ -67,9 +66,10 @@ set_base_aes_specs <- function(plot) {
 
   plot + 
     scale_y_continuous(expand = c(0, 0), # So X-axis set at 0
-                      limits = c(0, 300),
-                      sec.axis = sec_axis(~./(300/40), # Create secondary axis that is set based off of incidence axis
-                                          name = "Percent of TB cases with confirmed Xpert result", 
+                      limits = c(0, 40), 
+                      labels = scales::label_percent(scale = 1),
+                      sec.axis = sec_axis(~./(40/100), # Create secondary axis that is set based off of incidence axis
+                                          name = "Percent of TB cases with confirmed Xpert result",
                                           labels = scales::label_percent(scale = 1) # Format labels as percentages
                       )
     ) +  
@@ -77,23 +77,16 @@ set_base_aes_specs <- function(plot) {
       date_breaks = "1 year",  # Set breaks to 1 year
       date_labels = "%b %Y"  # Format labels as year
     ) + 
-    xlab("Time (Quarter)") + 
-    ylab("Incidence per 1,000 TB cases") + 
+    xlab("Quarter") + 
+    ylab("RR-TB Positivity") + 
     theme_bw() + 
     theme(
       axis.text.x  = element_text(size = 10), 
       axis.text.y  = element_text(size = 9), 
       legend.text = element_text(size = 12), 
       title = element_text(size = 12)) +
-    labs(
-      size = "Number of cases with conclusive Xpert result",
-      color = "Case Type"
-    ) + 
-    scale_size(
-      range = c(0.5, 5)
-    ) + 
     scale_color_manual(
-      name = "Case Type",
+      name = "Case type",
       labels = c(
         "New",
         "Previous"
@@ -102,21 +95,18 @@ set_base_aes_specs <- function(plot) {
         "black",
                "red"
       )
-    )
+      ) + 
+    scale_size( 
+      name = "Number of cases with conclusive Xpert result",
+      range = c(0.5, 5)
+    ) 
+    
   
 }
 
 
 
 # Plot in ggplot ----------------------------------------------------------
-
-
-fig_observed_trends <- plot_observed("new", "prev") %>% 
-  set_base_aes_specs() + 
-  ggtitle ("Observed RR-TB testing and trends")
-
-
-
 fig_sp <- plot_observed(quarter = "2014-01-01", "new", "prev") %>%  
   plot_model(agg_level = "nat_qrt", model_name = "sp_2014-2019") %>% 
   plot_model(agg_level = "nat_qrt", model_name = "sp_2015-2019") %>% 
@@ -126,39 +116,23 @@ fig_sp <- plot_observed(quarter = "2014-01-01", "new", "prev") %>%
                                    "sp_2015-2019" = "2015-2019 (Q12 dropped)"),
                         values=c("sp_2014-2019" = "solid",
                                  "sp_2015-2019" = "dashed"
-                                 )) + 
-  ggtitle ("Spatial models")
+                                 ))
   
 
 # Sensitivity analyses 
-fig_se1 <- 
+fig_sens <- 
   plot_observed(quarter = "2015-01-01", "new", "prev") %>%
   plot_model(agg_level = "nat_qrt", model_name = "sp_2015-2019") %>%
   plot_model(agg_level = "nat_qrt", model_name = "se1_sp_2015-2019") %>% 
-  set_base_aes_specs() + 
-  scale_linetype_manual(name = "Model", 
-                        labels = c("sp_2015-2019" = "Spatial (Ref)",
-                          "se1_sp_2015-2019" = "Spatial (Sens: Patient covs)"),
-                        values=c("sp_2015-2019" = "solid", 
-                          "se1_sp_2015-2019" = "dashed"
-                        )) + 
-  ggtitle ("Sensitivity: Additional patient covariates")
-
-
-fig_se2 <- plot_observed(quarter = "2015-01-01", "new", "prev") %>%  
-  plot_model(agg_level = "nat_qrt", model_name = "sp_2015-2019") %>%
   plot_model(agg_level = "nat_qrt", model_name = "se2_sp_2015-2019") %>% 
   set_base_aes_specs() + 
   scale_linetype_manual(name = "Model", 
-                        labels = c("sp_2015-2019" = "Spatial (Ref)", 
-                                   "se2_sp_2015-2019" = "Spatial (Sens: Interaction)"),
+                        labels = c("sp_2015-2019" = "Reference",
+                          "se1_sp_2015-2019" = "Additional patient covariates", 
+                          "se2_sp_2015-2019" = "Time varying selection"),
                         values=c("sp_2015-2019" = "solid", 
-                                 "se2_sp_2015-2019" = "dashed"
-                        )) + 
-  ggtitle ("Sensitivity: Time*Patient Covariates")
-
-  
-
+                          "se1_sp_2015-2019" = "dashed", 
+                          "se2_sp_2015-2019" = "dotted"))
   
   
 
@@ -167,11 +141,8 @@ fig_se2 <- plot_observed(quarter = "2015-01-01", "new", "prev") %>%
 
 
 # Save plots --------------------------------------------------------------
-ggsave(fig_observed_trends, filename = "output/figures_and_tables/fig_observed_trends.png", width = 14, height = 6)
-# ggsave(fig_tt, filename = "output/figures_and_tables/fig_tt.png", width = 14, height = 6)
 ggsave(fig_sp, filename = "output/figures_and_tables/fig_sp.png", width = 14, height = 6)
-ggsave(fig_se1, filename = "output/figures_and_tables/fig_se1.png", width = 14, height = 6)
-ggsave(fig_se2, filename = "output/figures_and_tables/fig_se2.png", width = 14, height = 6)
+ggsave(fig_sens, filename = "output/figures_and_tables/fig_sens.png", width = 14, height = 6)
 
 
 
@@ -184,221 +155,221 @@ ggsave(fig_se2, filename = "output/figures_and_tables/fig_se2.png", width = 14, 
 
 # Base R Attempt... -------------------------------------------------------
 
-
-
-
-png("output/figures_and_tables/fig_national_trends_combined.png", width = 8000, height = 6000, units = "px", res = 1000) 
-
+# 
+# 
+# 
+# png("output/figures_and_tables/fig_national_trends_combined.png", width = 8000, height = 6000, units = "px", res = 1000) 
+# 
+# # png("output/figures_and_tables/fig_observed_trends.png", width = 8000, height = 6000, units = "px", res = 1000) 
+# 
+# 
+# plot_observed_data <- function () {
+#   par(mar = c(5, 4, 4, 6) + 0.1)
+#   
+#   ## New cases ---------------------------------------------------------------
+#   data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2014-2019" & case_type == "new")
+#   
+#   
+#   ## Plot observed cases that are RR-TB positive
+#   
+#   plot(data$diag_qrt, (data$obs_pct_pos) * 100,
+#        pch = 19,
+#        cex = 0.6,
+#        axes = FALSE,
+#        ylim = c(0, 35),
+#        xlab = "",
+#        ylab = "", 
+#        col = "black"
+#   )
+#   
+#   axis(2, ylim = c(0, 35), col = "black", las = 1)
+#   mtext("Percent RR-TB positive", side = 2, line = 2.5)
+#   box()
+#   
+#   
+#   
+#   ## Add line for percent of new cases being tested with Xpert
+#   par(new = TRUE)
+#   
+#   plot(data$diag_qrt, (data$obs_pct_tested) * 100,
+#        xlab = "", 
+#        ylab = "", 
+#        ylim = c(0, 50),
+#        axes = FALSE, 
+#        col = "black", 
+#        type = "l"
+#   )
+#   
+#   axis(4, ylim = c(0, 50), col = "black", col.axis = "black", las = 1)
+#   mtext("Percent tested with Xpert", side = 4, col = "black", line = 4)
+#   
+#   
+#   
+#   ## Add X-axis
+#   axis(1, data$diag_qrt, format(data$diag_qrt, "%b %Y"))
+#   mtext("Time (Quarters)", side = 1, col = "black", line = 2.5)
+#   
+#   
+#   
+#   
+#   
+#   
+#   # Previous cases ----------------------------------------------------------
+#   data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2014-2019" & case_type == "prev")
+#   
+#   
+#   ## Plot observed cases that are RR-TB positive
+#   par(new = TRUE)
+#   
+#   plot(data$diag_qrt, (data$obs_pct_pos) * 100,
+#        pch = 19,
+#        cex = 0.6,
+#        axes = FALSE,
+#        ylim = c(0, 35),
+#        xlab = "",
+#        ylab = "",
+#        col = "#A50F15"
+#   )
+#   
+#   ## Plot share of previous cases being tested
+#   par(new = TRUE)
+#   
+#   plot(data$diag_qrt, (data$obs_pct_tested) * 100,
+#        pch = 15, 
+#        xlab = "", 
+#        ylab = "", 
+#        ylim = c(0, 50),
+#        axes = FALSE, 
+#        col = "#A50F15", 
+#        type = "l"
+#   )
+#   
+#   
+#   
+# }
+# 
+# 
+# plot_new_ests <- function() {
+#   
+#   
+#   ## Model: TT 2014-2019
+#   par(new = TRUE)
+#   
+#   plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#        pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
+#        axes = FALSE, col = "blue", type = "l"
+#   )
+#   
+#   ## Model: TT 2015-2019
+#   par(new = TRUE)
+#   
+#   data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2015-2019" & case_type == "new")
+#   
+#   plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#        pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
+#        axes = FALSE, col = "blue", type = "l", lty = 2
+#   )
+#   
+#   ## Model: SP 2014-2019
+#   par(new = TRUE)
+#   
+#   data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "new")
+#   
+#   plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#        pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
+#        axes = FALSE, col = "purple", type = "l"
+#   )
+#   
+#   ## Model: SP 2015-2019
+#   par(new = TRUE)
+#   
+#   data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2015-2019" & case_type == "new")
+#   
+#   plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#        pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
+#        axes = FALSE, col = "purple", type = "l", lty = 2
+#   )
+#   
+#   
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# # Plot observed data only 
 # png("output/figures_and_tables/fig_observed_trends.png", width = 8000, height = 6000, units = "px", res = 1000) 
-
-
-plot_observed_data <- function () {
-  par(mar = c(5, 4, 4, 6) + 0.1)
-  
-  ## New cases ---------------------------------------------------------------
-  data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2014-2019" & case_type == "new")
-  
-  
-  ## Plot observed cases that are RR-TB positive
-  
-  plot(data$diag_qrt, (data$obs_pct_pos) * 100,
-       pch = 19,
-       cex = 0.6,
-       axes = FALSE,
-       ylim = c(0, 35),
-       xlab = "",
-       ylab = "", 
-       col = "black"
-  )
-  
-  axis(2, ylim = c(0, 35), col = "black", las = 1)
-  mtext("Percent RR-TB positive", side = 2, line = 2.5)
-  box()
-  
-  
-  
-  ## Add line for percent of new cases being tested with Xpert
-  par(new = TRUE)
-  
-  plot(data$diag_qrt, (data$obs_pct_tested) * 100,
-       xlab = "", 
-       ylab = "", 
-       ylim = c(0, 50),
-       axes = FALSE, 
-       col = "black", 
-       type = "l"
-  )
-  
-  axis(4, ylim = c(0, 50), col = "black", col.axis = "black", las = 1)
-  mtext("Percent tested with Xpert", side = 4, col = "black", line = 4)
-  
-  
-  
-  ## Add X-axis
-  axis(1, data$diag_qrt, format(data$diag_qrt, "%b %Y"))
-  mtext("Time (Quarters)", side = 1, col = "black", line = 2.5)
-  
-  
-  
-  
-  
-  
-  # Previous cases ----------------------------------------------------------
-  data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2014-2019" & case_type == "prev")
-  
-  
-  ## Plot observed cases that are RR-TB positive
-  par(new = TRUE)
-  
-  plot(data$diag_qrt, (data$obs_pct_pos) * 100,
-       pch = 19,
-       cex = 0.6,
-       axes = FALSE,
-       ylim = c(0, 35),
-       xlab = "",
-       ylab = "",
-       col = "#A50F15"
-  )
-  
-  ## Plot share of previous cases being tested
-  par(new = TRUE)
-  
-  plot(data$diag_qrt, (data$obs_pct_tested) * 100,
-       pch = 15, 
-       xlab = "", 
-       ylab = "", 
-       ylim = c(0, 50),
-       axes = FALSE, 
-       col = "#A50F15", 
-       type = "l"
-  )
-  
-  
-  
-}
-
-
-plot_new_ests <- function() {
-  
-  
-  ## Model: TT 2014-2019
-  par(new = TRUE)
-  
-  plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-       pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
-       axes = FALSE, col = "blue", type = "l"
-  )
-  
-  ## Model: TT 2015-2019
-  par(new = TRUE)
-  
-  data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2015-2019" & case_type == "new")
-  
-  plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-       pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
-       axes = FALSE, col = "blue", type = "l", lty = 2
-  )
-  
-  ## Model: SP 2014-2019
-  par(new = TRUE)
-  
-  data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "new")
-  
-  plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-       pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
-       axes = FALSE, col = "purple", type = "l"
-  )
-  
-  ## Model: SP 2015-2019
-  par(new = TRUE)
-  
-  data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2015-2019" & case_type == "new")
-  
-  plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-       pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
-       axes = FALSE, col = "purple", type = "l", lty = 2
-  )
-  
-  
-}
-
-
-
-
-
-
-# Plot observed data only 
-png("output/figures_and_tables/fig_observed_trends.png", width = 8000, height = 6000, units = "px", res = 1000) 
-plot_observed_data()
-dev.off()
-
-
-
-
-# Plot observed data and modeled data 
-png("output/figures_and_tables/fig_national_trends_combined.png", width = 8000, height = 6000, units = "px", res = 1000) 
-plot_observed_data()
-plot_new_ests()
-dev.off()
-
-
-
-
-
-
-## Plot model estimates for new cases --------------------------------------
-
-
-dev.off()
-
-
-## Plot observed data for previous cases -----------------------------------
-
-
-
-## Plot model estimates for previous cases ---------------------------------
-
-## Model: TT 2014-2019
-par(new = TRUE)
-
-plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-     pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
-     axes = FALSE, col = "blue", type = "l"
-)
-
-
-## Model: TT 2015-2019
-par(new = TRUE)
-
-data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2015-2019" & case_type == "prev")
-
-plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-     pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
-     axes = FALSE, col = "blue", type = "l", lty = 2
-)
-
-
-## Model: SP 2014-2019
-data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "prev")
-
-par(new = TRUE)
-
-plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-     pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
-     axes = FALSE, col = "purple", type = "l"
-)
-
-
-## Model: SP 2015-2019
-data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "prev")
-
-par(new = TRUE)
-
-plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
-     pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
-     axes = FALSE, col = "purple", type = "l", lty = 2
-)
-
+# plot_observed_data()
+# dev.off()
+# 
+# 
+# 
+# 
+# # Plot observed data and modeled data 
+# png("output/figures_and_tables/fig_national_trends_combined.png", width = 8000, height = 6000, units = "px", res = 1000) 
+# plot_observed_data()
+# plot_new_ests()
+# dev.off()
+# 
+# 
+# 
+# 
+# 
+# 
+# ## Plot model estimates for new cases --------------------------------------
+# 
+# 
+# dev.off()
+# 
+# 
+# ## Plot observed data for previous cases -----------------------------------
+# 
+# 
+# 
+# ## Plot model estimates for previous cases ---------------------------------
+# 
+# ## Model: TT 2014-2019
+# par(new = TRUE)
+# 
+# plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#      pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
+#      axes = FALSE, col = "blue", type = "l"
+# )
+# 
+# 
+# ## Model: TT 2015-2019
+# par(new = TRUE)
+# 
+# data <- compiled_results[["nat_qrt"]] %>% filter(model == "tt_2015-2019" & case_type == "prev")
+# 
+# plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#      pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
+#      axes = FALSE, col = "blue", type = "l", lty = 2
+# )
+# 
+# 
+# ## Model: SP 2014-2019
+# data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "prev")
+# 
+# par(new = TRUE)
+# 
+# plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#      pch = 15, xlab = "", ylab = "", ylim = c(0, 35),
+#      axes = FALSE, col = "purple", type = "l"
+# )
+# 
+# 
+# ## Model: SP 2015-2019
+# data <- compiled_results[["nat_qrt"]] %>% filter(model == "sp_2014-2019" & case_type == "prev")
+# 
+# par(new = TRUE)
+# 
+# plot(data$diag_qrt, (data$fitted_RR / data$total_TB_cases) * 100,
+#      pch = 15, xlab = "", ylab = "", ylim = c(0, 35), xlim = as.Date(c("2015-01-01", "2019-10-01")),
+#      axes = FALSE, col = "purple", type = "l", lty = 2
+# )
+# 
 
 
 
