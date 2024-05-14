9aa3b918907588260b65ffd7badc403716a1c1d1
sbaum95 <sbaum@g.harvard.edu>
2024-05-14 14:00:46 -0400

    update model object names; Removed health unit (need to add back in when you get it from Dani); Added relative file version

--- code/02_run_models/01_run_models.R
+++ code/02_run_models/01_run_models.R
@@ -1,12 +1,14 @@
 # Author: Sarah Baum
 # Created: 2024-03-22
-# Updated:
+# Updated: 2024-05-13
+
 
 # Description: Run all models
 library(rlang)
 
 # Create model functions ---------------------------------------------------
 run_sp_model <- function(name, data, filter_expr, outcome, covariates, latitude, longitude, k_t, k_sp) {
+  
   # Prepare data
   model_df <- data %>%
     group_by(across(all_of(covariates)), result, time, diag_qrt, state, lat, lon) %>%
@@ -106,93 +108,100 @@ run_sel_model <- function(name, data, filter_expr, outcome, covariates, latitude
 
 
 # Run models and store fitted results -------------------------------------
-fitted_models <- list() # To store model output
-
 
 
 # Spatial Models ----------------------------------------------------------
 
-## New Cases: 2014-2019
+## 2014-
+if ("sp_2014" %in% models_to_run){
+  
+## New Cases
 tictoc::tic()
-fitted_models[["sp_2014-2019_new"]] <- run_sp_model(
+fitted_models[["sp_2014_new"]] <- run_sp_model(
   data = mdf_new_ind,
   filter_expr = quo(),
   outcome = "cbind(positive, negative)",
-  covariates = c("age_cat", "hiv_status", "sex", "health_unit"),
+  covariates = c("age_cat", "hiv_status", "sex"),
   latitude = "lat",
   longitude = "lon",
-  k_t = 23,
+  k_t = 30,
   k_sp = 50
 )
 
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
 tictoc::toc()
 
 
-
-
-## New Cases: 2015-2019
+## Previous Cases
 tictoc::tic()
-fitted_models[["sp_2015-2019_new"]] <- run_sp_model(
-  data = mdf_new_ind %>% filter(time >= 5),
-  outcome = "cbind(positive, negative)",
-  # filter out quarter 12 
-  filter_expr = quo(time != 12),
-  covariates = c("age_cat", "hiv_status", "sex", "health_unit"),
-  latitude = "lat", 
-  longitude = "lon",
-  k_t = 19,
-  k_sp = 50
-)
-
-save(fitted_models, file = model_file_name)
-tictoc::toc()
-
-
-
-## Previous Cases: 2014-2019
-tictoc::tic()
-fitted_models[["sp_2014-2019_prev"]] <- run_sp_model(
+fitted_models[["sp_2014_prev"]] <- run_sp_model(
   data = mdf_prev_ind,
   filter_expr = quo(),
   outcome = "cbind(positive, negative)",
-  covariates = c("tratamento","age_cat", "hiv_status", "sex", "health_unit"),
+  covariates = c("tratamento","age_cat", "hiv_status", "sex"),
   latitude = "lat",
   longitude = "lon",
-  k_t = 23,
+  k_t = 30,
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
+
+
+}
+
+
+if ("sp_2015" %in% models_to_run){
+  
+## New Cases
+# tictoc::tic()
+# fitted_models[["sp_2015_new"]] <- run_sp_model(
+#   data = mdf_new_ind %>% filter(time >= 5),
+#   outcome = "cbind(positive, negative)",
+#   # filter out quarter 12 
+#   filter_expr = quo(time != 12),
+#   covariates = c("age_cat", "hiv_status", "sex"),
+#   latitude = "lat", 
+#   longitude = "lon",
+#   k_t = 30,
+#   k_sp = 50
+# )
+# 
+# save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
+# tictoc::toc()
 
 
 
-## Previous Cases: 2015-2019
+
+## Previous Cases
 tictoc::tic()
-fitted_models[["sp_2015-2019_prev"]] <- run_sp_model(
+fitted_models[["sp_2015_prev"]] <- run_sp_model(
   data = mdf_prev_ind %>% filter(time >= 5),
   filter_expr = quo(time != 12),
   outcome = "cbind(positive, negative)",
-  covariates = c("tratamento", "age_cat", "sex", "hiv_status", "health_unit"),
+  covariates = c("tratamento", "age_cat", "sex", "hiv_status"),
   latitude = "lat", 
   longitude = "lon",
   k_t = 19,
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
+
+}
 
 # Sensitivity Analyses ----------------------------------------------------
 
 
 ## 1. Additional patient covariates ----------------------------------------
 
+if ("sens_1" %in% models_to_run){
 tictoc::tic()
-fitted_models[["se1_sp_2015-2019_new"]] <- run_sp_model(
+fitted_models[["sens_1_new"]] <- run_sp_model(
   data = mdf_new_ind %>% filter(time >= 5),
   filter_expr = quo(time != 12),
   outcome = "cbind(positive, negative)",
-  covariates = c("age_cat", "sex", "hiv_status", "health_unit",
+  covariates = c("age_cat", "sex", "hiv_status",
                  "pop_rua", "pop_liber", "agravtabac", "agravalcoo", "agravdroga", 
                  "agravdiabe", "cs_escol_n", "pop_imig"),
   latitude = "lat", 
@@ -201,16 +210,16 @@ fitted_models[["se1_sp_2015-2019_new"]] <- run_sp_model(
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
 
 
 
 tictoc::tic()
-fitted_models[["se1_sp_2015-2019_prev"]] <- run_sp_model(
+fitted_models[["sens_1_prev"]] <- run_sp_model(
   data = mdf_prev_ind %>% filter(time >= 5),
   filter_expr = quo(time != 12),
   outcome = "cbind(positive, negative)",
-  covariates = c("age_cat", "sex", "hiv_status", "health_unit","tratamento",
+  covariates = c("age_cat", "sex", "hiv_status", "tratamento",
                  "pop_rua", "pop_liber", "agravtabac", "agravalcoo", "agravdroga", 
                  "agravdiabe", "cs_escol_n", "pop_imig"),
   latitude = "lat", 
@@ -219,184 +228,43 @@ fitted_models[["se1_sp_2015-2019_prev"]] <- run_sp_model(
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
 
-
+}
 
 
 ## 2. Selection overtime ---------------------------------------------------
+if ("sens_2" %in% models_to_run){
 tictoc::tic()
-fitted_models[["se2_sp_2015-2019_new"]] <- run_sel_model(
+fitted_models[["sens_2_new"]] <- run_sel_model(
   data = mdf_new_ind %>% filter(time >= 5),
   filter_expr = quo(time != 12), 
   outcome = "cbind(positive, negative)",
-  covariates = c("age_cat", "sex", "hiv_status", "health_unit"),
+  covariates = c("age_cat", "sex", "hiv_status"),
   latitude = "lat", 
   longitude = "lon",
   k_t = 19,
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
 
 tictoc::tic()
-fitted_models[["se2_sp_2015-2019_prev"]] <- run_sel_model(
+fitted_models[["sens_2_prev"]] <- run_sel_model(
   data = mdf_prev_ind %>% filter(time >= 5),
   filter_expr = quo(time != 12),
   outcome = "cbind(positive, negative)",
-  covariates = c("tratamento", "age_cat", "sex", "hiv_status", "health_unit"),
+  covariates = c("tratamento", "age_cat", "sex", "hiv_status"),
   latitude = "lat", 
   longitude = "lon",
   k_t = 19,
   k_sp = 50
 )
 tictoc::toc()
-save(fitted_models, file = model_file_name)
+save(fitted_models, file = paste0("fitted_models_", file_version, ".Rdata"))
+
+}
 
 
 
 
-
-
-
-
-
-
-# Store model output ------------------------------------------------------
-save(fitted_models, file = model_file_name)
-
-
-
-
-# Appendix: Time trend models ---------------------------------------------
-## Create time trend model with patient and municipality covariates
-# run_tt_model <- function(name, data, filter_expr, outcome, covariates, k) {
-#   
-#   # Prepare data 
-#   model_df <- data %>% 
-#     group_by(across(all_of(covariates)), result, time, diag_qrt, state, id_municip) %>% 
-#     count() %>% 
-#     pivot_wider(names_from = "result", values_from = "n") %>%
-#     rename(neg = "Negative",
-#            pos = "Positive",
-#            miss = "Missing") %>%
-#     mutate(Negative = if_else(is.na(neg), 0, neg),
-#            Positive = if_else(is.na(pos), 0, pos),
-#            Miss = if_else(is.na(miss), 0, miss)) %>% 
-#     mutate(cases = Negative + Positive + Miss, 
-#            pct_tested = (Negative + Positive)/(Negative + Positive + Miss), 
-#            obs_pct_positive = if_else(is.nan(Positive/(Negative + Positive)), 0, Positive/(Negative + Positive))
-#     ) %>% 
-#     dplyr::select(-c(miss, neg, pos))
-#   
-#   
-#   # Write out model
-#   formula_string <- paste(outcome, "~", paste(covariates, collapse = " + "), "+ s(state, bs = 're') + s(time, k =", paste(k), ") + s(time, by = state, id = 1, k =", paste(k), ")")
-#   
-#   # Run model
-#   model_object <- bam(as.formula(formula_string),
-#                       data = if (!quo_is_missing(filter_expr)) {
-#                         model_df %>% filter(!!filter_expr)
-#                       } else {
-#                         model_df
-#                       },
-#                       family = binomial(link = "logit"),
-#                       method = "REML"
-#   )
-#   
-#   return(list(model_object, 
-#               model_df))
-# }
-# 
-# 
-# 
-# 
-# ## New Cases: 2014-2019
-# tictoc::tic()
-# fitted_models[["tt_2014-2019_new"]] <- run_tt_model(
-#   data = mdf_new_ind, 
-#   filter_expr = NA, 
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("age_cat", "hiv_status", "sex", "health_unit", "mun_has_prison", "mun_urban_cat", "mun_bf_cat", "mun_fhs_cat", "border"),
-#   k = 23
-# )
-# 
-# tictoc::toc()
-# 
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
-# 
-# 
-# ## New Cases: 2015-2019, dropping quarter 12
-# tictoc::tic()
-# fitted_models[["tt_2015-2019_new"]] <- run_tt_model(
-#   data = mdf_new_ind %>% filter(sex != "Missing") %>% filter(time >= 5),
-#   filter_expr = filter(time !=12),
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("age_cat", "hiv_status", "sex", "health_unit", "mun_has_prison", "mun_urban_cat", "mun_bf_cat", "mun_fhs_cat", "border"),
-#   k = 19
-# )
-# tictoc::toc()
-# 
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
-# 
-# 
-# ## Previous Cases: 2014-2019
-# tictoc::tic()
-# fitted_models[["tt_2014-2019_prev"]] <- run_tt_model(
-#   data = mdf_prev_ind,
-#   filter_expr = NA, 
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("tratamento", "age_cat", "hiv_status", "sex", "health_unit", "mun_has_prison", "mun_urban_cat", "mun_bf_cat", "mun_fhs_cat", "border"),
-#   k = 23
-# )
-# tictoc::toc()
-# 
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
-# 
-# 
-# 
-# tictoc::tic()
-# ## Time trend model: 2015-2019, dropping quarter 12
-# fitted_models[["tt_2015-2019_prev"]] <- run_tt_model(
-#   data = mdf_prev_ind %>% filter(time >= 5),
-#   filter_expr = filter(time != 12),
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("tratamento", "age_cat", "hiv_status", "sex", "health_unit", "mun_has_prison", "mun_urban_cat", "mun_bf_cat", "mun_fhs_cat", "border"),
-#   k = 19
-# )
-# tictoc::toc()
-# 
-# 
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
-# 
-# 
-
-# Sensitivity Analyses 
-
-
-## 1. Additional patient covariates 
-# tictoc::tic()
-# fitted_models[["se1_tt_2015-2019_new"]] <- run_tt_model(
-#   data = mdf_new_ind %>% filter(time >= 5) %>% filter(sex != "Missing"),
-#   filter_expr = filter(time != 12),
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("age_cat", "sex", "hiv_status", "health_unit",
-#                  "mun_urban_cat", "mun_has_prison", "mun_bf_cat", "mun_fhs_cat",
-#                  "pop_rua", "pop_liber", "agravtabac", "agravalcoo", "agravdroga", "agravdiabe"),
-#   k = 19
-# )
-# tictoc::toc()
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
-
-# tictoc::tic()
-# fitted_models[["se1_tt_2015-2019_prev"]] <- run_tt_model(
-#   data = mdf_prev_ind %>% filter(time >= 5) %>% filter(age_cat != "0-4"),
-#   filter_expr = filter(time != 12),
-#   outcome = "cbind(Positive, Negative)",
-#   covariates = c("age_cat", "sex", "hiv_status", "health_unit", "tratamento",
-#                  "mun_urban_cat", "mun_has_prison", "mun_bf_cat", "mun_fhs_cat", 
-#                  "pop_rua", "pop_liber", "agravtabac", "agravalcoo", "agravdroga", "agravdiabe"),
-#   k = 19
-# )
-# tictoc::toc()
-# save(fitted_models, file = "output/fitted_models.tmp.Rdata")
