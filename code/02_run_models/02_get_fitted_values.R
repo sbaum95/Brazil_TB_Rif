c037050fa159bc2bad50df1a6a16f8c58f5c66ef
sbaum95 <sbaum@g.harvard.edu>
2024-05-14 14:01:11 -0400

    fixed fitted values call

--- code/02_run_models/02_get_fitted_values.R
+++ code/02_run_models/02_get_fitted_values.R
@@ -18,7 +18,7 @@
 get_fitted_values <- function (model_name) {
   
   # Fit values 
-  fitted_values <- fitted_values(fitted_models[[model_name]][[1]], 
+  fitted_values <- gratia::fitted_values(fitted_models[[model_name]][[1]], 
                                  data = fitted_models[[model_name]][[2]], 
                                  scale = "response")
   
@@ -36,9 +36,3 @@ model_name <- names(fitted_models)
 fitted_values_list <- lapply(model_name, get_fitted_values) 
 
 fitted_values <- setNames(fitted_values_list, model_name)
-
-
-# Store fitted values -----------------------------------------------------
-save(fitted_values, file = fitted_file_name)
-
-
