51a3cca92c2555c475e76c1d4cd2b111778a8703
sbaum95 <sbaum@g.harvard.edu>
2024-05-30 11:42:41 -0700

    change to y-axis to person-years, palette, and scales

--- code/03_make_figures_and_tables/fig_state_trends.R
+++ code/03_make_figures_and_tables/fig_state_trends.R
@@ -1,18 +1,17 @@
 
 # Create function to get plots by state-quarter ---------------------------
 fig_state <- function(state_list, model_name, case_type1 = NULL, case_type2 = NULL) {
-  
   data <- compiled_results[["state_qrt"]] %>%
     # left_join(., pop_UF, by = c("state_nm" = "state")) %>%
-    mutate(tb_inc = (total_TB_cases / pop_2010) * 100000) %>%
+    mutate(tb_inc = (total_TB_cases / pop_2010) * 4 * 100000) %>%
     filter(state_nm %in% state_list) %>%
     filter(model == model_name) %>%
     group_by(diag_qrt, state_nm) %>%
     mutate(
-      obs_inc = if_else(obs_RR != 0, (((obs_RR / obs_num_tested) * total_TB_cases) / pop_2010) * 100000, 0),
-      mod_inc = (fitted_RR / pop_2010) * 100000,
-      inc_lci = (proj_lci / pop_2010) * 100000,
-      inc_hci = (proj_hci / pop_2010) * 100000
+      obs_inc = if_else(obs_RR != 0, (((obs_RR / obs_num_tested) * total_TB_cases) / pop_2010) * 4 * 100000, 0),
+      mod_inc = (fitted_RR / pop_2010) * 4 * 100000,
+      inc_lci = (proj_lci / pop_2010) * 4 * 100000,
+      inc_hci = (proj_hci / pop_2010) * 4 * 100000
     )
 
   data$facet_state <- factor(data$state_nm, levels = state_list)
@@ -29,27 +28,30 @@ fig_state <- function(state_list, model_name, case_type1 = NULL, case_type2 = NU
 
 
   ggplot(data_plot) +
-
-    # Add line for projected incidence for each model (2017-2019)
-    geom_line(aes(x = diag_qrt, y = obs_inc, color = "Observed")) +
-
     # Add line for projected incidence for each model (2017-2019)
     geom_line(aes(x = diag_qrt, y = mod_inc, color = "Projected")) +
-    geom_errorbar(aes(x = diag_qrt, ymin = inc_lci, ymax = inc_hci, color = "Projected"), alpha = 0.5) +
-    facet_wrap(~facet_state, scales = "free")
+    geom_ribbon(aes(x = diag_qrt, ymin = inc_lci, ymax = inc_hci, color = "Projected"), alpha = 0.5, fill = "lightgray", color = NA) +
+    # Add line for observed incidence for each model (2017-2019)
+    geom_line(aes(x = diag_qrt, y = obs_inc, color = "Observed")) + 
+    scale_y_continuous(expand = c(0,0), 
+                       breaks = scales::pretty_breaks(n=5)) + 
+    facet_wrap(~facet_state)
+
+   
 }
 
 
-set_base_aes_specs <- function(by_state) {
+set_base_aes_specs <- function(by_state, case_type1) {
+  
+  if(case_type1 == "new") {
   by_state +
-    scale_y_continuous(expand = c(0,0)) + 
-
     scale_x_date(
       date_breaks = "1 year", # Set breaks to 1 year
       date_labels = "%Y" # Format labels as year
     ) +
-    xlab("Quarter") +
-    ylab("Incidence per 100,000 population") +
+    xlab("Year") +
+    ylab("RR-TB cases per 100,000 person-years") +
+    ggtitle("A) New Cases") + 
     theme_bw() +
     theme(
       axis.text.x = element_text(size = 7),
@@ -67,16 +69,47 @@ set_base_aes_specs <- function(by_state) {
         "Observed",
         "Projected"
       ),
-      values = c("red", "black")
-    )
-}
+      values = c(pal[1], pal[3])
+    )}
+  
+  else{by_state +
+    
+      
+      scale_x_date(
+        date_breaks = "1 year", # Set breaks to 1 year
+        date_labels = "%Y" # Format labels as year
+      ) +
+      xlab("Year") +
+      ylab("RR-TB cases per 100,000 person-years") +
+      ggtitle("B) Previously Treated Cases") + 
+        theme_bw() +
+          theme(
+            axis.text.x = element_text(size = 7),
+            axis.text.y = element_text(size = 9),
+            legend.text = element_text(size = 9),
+            title = element_text(size = 10), 
+            strip.text = element_text(size = 10)
+          ) +
+          scale_size(
+            range = c(0.5, 5)
+          ) +
+          scale_color_manual(
+            name = "",
+            labels = c(
+              "Observed",
+              "Projected"
+            ),
+            values = c(pal[1], pal[3])
+          )}
+    
+  }
+  
 
 
 
 # Execute function --------------------------------------------------------
 state_list_new <- compiled_results[["state_yr"]] %>%
-  # left_join(., pop_UF, by = c("state_nm" = "state")) %>%
-  filter(year == 2023 & case_type == "new" & model == "sp_2015") %>%
+  filter(year == 2023 & case_type == "new" & model == "sp_2017" & obs_pct_tested > 0.3) %>%
   mutate(
     tb_inc = (total_TB_cases / pop_2010) * 100000,
     rr_inc = (fitted_RR / pop_2010) * 100000
@@ -88,14 +121,13 @@ state_list_new <- compiled_results[["state_yr"]] %>%
   pull()
 
 
-fig_state_new_sp <- fig_state(state_list = state_list_new, model_name = "sp_2015", case_type1 = "new", case_type2 = NULL) %>%
-  set_base_aes_specs()
+fig_state_new_sp <- fig_state(state_list = state_list_new, model_name = "sp_2017", case_type1 = "new", case_type2 = NULL) %>%
+  set_base_aes_specs(case_type1 = "new")
 
 
 state_list_prev <- compiled_results[["state_yr"]] %>%
-  # left_join(., pop_UF, by = c("state_nm" = "state")) %>%
   mutate(tb_inc = (total_TB_cases / pop_2010) * 100000) %>%
-  filter(year == 2019 & case_type == "prev" & model == "sp_2015") %>%
+  filter(year == 2023 & case_type == "prev" & model == "sp_2017" & obs_pct_tested > 0.3) %>%
   mutate(
     tb_inc = (total_TB_cases / pop_2010) * 100000,
     rr_inc = (fitted_RR / pop_2010) * 100000
@@ -107,22 +139,6 @@ state_list_prev <- compiled_results[["state_yr"]] %>%
   pull()
 
 
-fig_state_prev_sp <- fig_state(state_list = state_list_prev, model_name = "sp_2015", case_type1 = "prev") %>%
-  set_base_aes_specs()
+fig_state_prev_sp <- fig_state(state_list = state_list_prev, model_name = "sp_2017", case_type1 = "prev") %>%
+  set_base_aes_specs(case_type1 = "prev")
 
-
-
-
-# Compare all models ------------------------------------------------------
-# model_list <- c("tt_2014-2019","tt_2015-2019", "sp_2014-2019", "sp_2015-2019", "se1_sp_2015-2019", "se2_sp_2015-2019")
-#
-# compiled_results[["state_qrt"]] %>%
-#   filter(state_nm %in% state_list, model %in% model_list, case_type == "new") %>%
-#   group_by(state_nm, diag_qrt, model) %>%
-#   summarize(mod_incidence = (fitted_RR/total_TB_cases) * 1000) %>%
-#   ggplot() +
-#
-#   # Add line for projected incidence for each model (2017-2019)
-#   geom_line(aes(x = diag_qrt, y = mod_incidence, color = model)) +
-#
-#   facet_wrap(~state_nm, scales = "free")
