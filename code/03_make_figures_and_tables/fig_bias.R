0056fa2af1ac435c0d9c815a9e2dc496e281fdf3
sbaum95 <sbaum@g.harvard.edu>
2024-06-04 14:34:30 -0700

    Change to prevalence instead of case counts

--- code/03_make_figures_and_tables/fig_bias.R
+++ code/03_make_figures_and_tables/fig_bias.R
@@ -1,38 +1,40 @@
 
 
-fig_bias <- ggplot() + 
+fig_bias <- ggplot(data = compiled_results[["nat_qrt"]] %>% 
+                     filter(model == "sp_2017") %>% 
+                     mutate(case_type = if_else(case_type == "new", "New", "Previously Treated")) %>% 
+                     group_by(diag_qrt, case_type) %>% 
+                     summarize(
+                       prev_mod = (fitted_RR*4/pop_2010)*100000,
+                               prev_mod_lci = (proj_lci*4/pop_2010)*100000,
+                               prev_mod_hci = (proj_hci*4/pop_2010)*100000,
+                               obs_pct_tested = obs_pct_tested, 
+                               prev_nav = ((obs_RR*(1/obs_pct_tested)*4)/pop_2010)*100000, 
+                               bias_prev = prev_mod/prev_nav, 
+                               bias_lci = prev_mod_lci/prev_nav, 
+                               bias_hci = prev_mod_hci/prev_nav)) + 
   # Quantify bias for new cases
   ## Plot point estimate 
-  geom_line(
-    data = compiled_results[["nat_qrt"]] %>% 
-      filter(model == "sp_2017") %>% 
-      group_by(case_type) %>% 
-      mutate(bias = fitted_RR/obs_RR),
-    aes(diag_qrt, bias, color = case_type)) + 
+  geom_line(aes(diag_qrt, bias_prev, color = case_type)) + 
   
   ## Plot error bar 
-  geom_ribbon(data = compiled_results[["nat_qrt"]] %>% 
-                  filter(model == "sp_2017") %>% 
-                  group_by(case_type) %>% 
-                  mutate(bias_lci = proj_lci/obs_RR, 
-                         bias_hci = proj_hci/obs_RR), 
-                aes(x = diag_qrt, ymin = bias_lci, ymax = bias_hci, group = case_type, fill = case_type), alpha = 0.2, color = NA) + 
+  geom_ribbon(aes(x = diag_qrt, ymin = bias_lci, ymax = bias_hci, group = case_type, fill = case_type), alpha = 0.2, color = NA) + 
   scale_y_continuous(expand = c(0,0), 
-                     limits = c(0,10)) + 
+                     limits = c(0.75,1.75)) + 
+  facet_wrap(~ case_type) + 
   scale_x_date(
     date_breaks = "1 year",  # Set breaks to 1 year
-    date_labels = "%b %Y"  # Format labels as year
+    date_labels = "%Y"  # Format labels as year
   ) + 
-  
+  geom_hline(yintercept = 1, linetype = "dashed", color = "black") + 
   xlab("Quarter") + 
-  ylab("Ratio of projected to observed") + 
+  ylab("Ratio of projected to naive prevalence") + 
   theme_bw() + 
   theme(strip.background = element_blank(),
-        strip.text = element_text(size = 11),
-        axis.text.x  = element_text(size = 12), 
-        axis.text.y  = element_text(size = 12), 
-        legend.text = element_text(size = 12), 
-        title = element_text(size = 14)) +
+        strip.text = element_text(size = 16),
+        axis.text.x  = element_text(size = 9), 
+        axis.text.y  = element_text(size = 9), 
+        legend.position = "none") +
   scale_color_manual(name="Case type",
                      labels=c("New",
                               "Previous"),
