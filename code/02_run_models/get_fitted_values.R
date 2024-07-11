70021e0eb6182beb819e191b2441ad23f6f1aafb
sbaum95 <sbaum@g.harvard.edu>
2024-07-11 14:42:45 -0700

    rename

---
+++ code/02_run_models/get_fitted_values.R
@@ -0,0 +1,25 @@
+# Author: Sarah Baum
+# Created: 2024-03-22
+
+# Description: Get fitted values from all models
+
+# Create function that store fitted values --------------------------------
+get_fitted_values <- function (model_name) {
+  
+  # Fit values 
+  fitted_values <- gratia::fitted_values(fitted_models[[model_name]][[1]], 
+                                 data = fitted_models[[model_name]][[2]], 
+                                 scale = "response")
+  
+  return(fitted_values)
+  
+}
+
+
+# # Get fitted values from models -------------------------------------------
+# 
+# model_name <- names(fitted_models)
+# 
+# fitted_values_list <- lapply(model_name, get_fitted_values) 
+# 
+# fitted_values <- setNames(fitted_values_list, model_name)
