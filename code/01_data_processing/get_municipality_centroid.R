c46fb03bfab11b4e63fdd0351571c77b1692284f
sbaum95 <sbaum@g.harvard.edu>
2024-07-11 13:58:06 -0700

    Tidy up code

--- code/01_data_processing/get_municipality_centroid.R
+++ code/01_data_processing/get_municipality_centroid.R
@@ -1,17 +1,11 @@
-# Author: Sarah Baum
-# Created: 2024-03-22
-# Updated: 
 # Description: Pulls municipality centroid from 2020 IBGE municipality centroid
 
 
 get_municipality_centroid <- function() {
+  
 # load shapefile
-# Source: Shapefile - IBGE; Projection - https://epsg.io/5875
-
+# Source: Shapefile - IBGE
 shp_mun <- st_read("data/BR_Municipios_2020/BR_Municipios_2020.shp") %>% st_simplify(dTolerance = 20)
-
-# shp_mun <- shp_mun %>% st_set_crs(value = 5527) 
-# %>% st_transform(crs = 5875) # Change to EPSG:4674 SAD69, UTM Zone 18S
 
 # Truncate municip ID (7-digit from IBGE to 6 in SINAN) to merge
 shp_mun$CD_MUN_merge <- as.numeric(substr(as.character(shp_mun$CD_MUN), 1, nchar(shp_mun$CD_MUN) - 1)) %>% as.factor()
