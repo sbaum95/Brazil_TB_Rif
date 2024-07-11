b29c26b3aa25a6f2b05066089bd30ebc599c53e2
sbaum95 <sbaum@g.harvard.edu>
2024-07-11 15:27:39 -0700

    Tidy comments, Output Table A1 to excel

--- code/03_make_figures_and_tables/01_run_all.R
+++ code/03_make_figures_and_tables/01_run_all.R
@@ -1,23 +1,19 @@
-# Author: Sarah Baum
-# Created: 2024-03-23
-# Updated: 2024-03-25
-
-# Description: Executes files to create all figures  
+# Description: Executes files to create all figures and tables
 
 source("code/dependencies.R")
 
 # Create file output params
-file_version_load <- "20240604"
-file_version_save <- "20240604"
+file_version_load <- "20240711"
+file_version_save <- "20240711"
 
-# Load relevant data  ------------------------------------------------
+# Load SINAN and model results  ------------------------------------------------
 load(paste0("output/compiled_results_", file_version_load, ".Rdata"))
 load(paste0("data/sinan_tmp_", file_version_load, ".Rdata"))
 load(paste0("data/mdf_new_ind_tmp_", file_version_load, ".Rdata"))
 load(paste0("data/mdf_prev_ind_tmp_", file_version_load, ".Rdata"))
 
-# Load national population ------------------------------------------------
 
+# Load population data ------------------------------------------------
 pop_UF <- read_excel("data/Brazil_population_UF.xls", skip = 6) %>%
   rename(
     pop_2010 = "...2",
@@ -31,7 +27,7 @@ pop_2010 = sum(pop_UF$pop_2010)
 
 
 
-# Figures  ------------------------------------------------
+# Create figures  ------------------------------------------------
 
 # Set palette
 pal <- ggsci::pal_npg("nrc", alpha = 0.8)(6)
@@ -45,46 +41,52 @@ observed_xpert_color = pal[2]
 who_color = "black"
 
 
-# Figure 1 - National plot with trends in testing and RR-TB positivity (And sensitivity analyses)
-source("code/03_make_figures_and_tables/fig_model_performance.R")
-ggsave(fig_model_performance, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_model_performance.png"), width = 14, height = 5)
-ggsave(fig_sens, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_sens.png"), width = 14, height = 5)
+# Figs 1 and 6 - Results for main model and alternative model specifications
+source("code/03_make_figures_and_tables/fig_model_results.R")
+ggsave(fig_main_results, filename = paste0("output/figures_and_tables/fig1_", file_version_save, "_main_results.png"), width = 14, height = 5)
+ggsave(fig_sens_results, filename = paste0("output/figures_and_tables/fig6_", file_version_save, "_sens_results.png"), width = 14, height = 5)
 
-# Figure 2 - Bias between model and observed
+# Fig 2 - Bias between model and observed
 source("code/03_make_figures_and_tables/fig_bias.R")
-ggsave(fig_bias, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_bias.png"), width = 10, height = 4)
+ggsave(fig_bias, filename = paste0("output/figures_and_tables/fig2_", file_version_save, "_bias.png"), width = 10, height = 4)
 
-# Figure 3 - National incidence + WHO comparisons
-source("code/03_make_figures_and_tables/fig_results_national_WHO.R")
-ggsave(fig_nat_imp, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_total_inc.png"), width = 14, height = 6)
+# Fig 3 - National total incidence and WHO comparisons
+source("code/03_make_figures_and_tables/fig_total_inc_WHO.R")
+ggsave(fig_total_inc_WHO, filename = paste0("output/figures_and_tables/fig3_", file_version_save, "_total_inc_WHO.png"), width = 14, height = 6)
 
-# Figure 3 - National incidence 
-# source("code/03_make_figures_and_tables/fig_national_results.R")
-# ggsave(fig_nat_imp, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_nat_imp.png"), width = 10, height = 5)    
-
-# Figure 4 - RR-TB incidence by state in 2023
+# Fig 4 - Levels by state (2023)
 source("code/03_make_figures_and_tables/fig_state_levels.R")
-ggsave(fig_combined_level, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_state_combined_level.png"), width = 16, height = 12)
+ggsave(fig_state_level, filename = paste0("output/figures_and_tables/fig4_", file_version_save, "_state_level.png"), width = 16, height = 12)
 
-# Figure 5 - Trends by state 
+# Fig 5 - Trends by state (2017-2023)
 source("code/03_make_figures_and_tables/fig_state_trends.R")
-ggsave(fig_state_trends, filename = paste0("output/figures_and_tables/fig_", file_version_save, "_state_trends.png"), width = 14, height = 6)
+ggsave(fig_state_trends, filename = paste0("output/figures_and_tables/fig5_", file_version_save, "_state_trends.png"), width = 14, height = 6)
 
 
 
 
 # Tables  ------------------------------------------------
 
-# Table 1 
-source("code/03_make_figures_and_tables/tab1_covs.R")
+# Table 1 - Descriptive characteristics of notified TB cases (2017-2023)
+source("code/03_make_figures_and_tables/tab1_descriptive.R")
+write_xlsx(dat_reordered, paste0("output/figures_and_tables/tab1_", file_version_save, "_descriptive.xlsx"))
 
-stargazer2(new_model_list)
-stargazer2(prev_model_list)
 
-# Missing count for Table 1 notes (there are no missing for health unit)
-tabyl(mdf_new_ind, sex)[[3, 2]] + tabyl(mdf_prev_ind, sex)[[3, 2]]
-tabyl(mdf_new_ind, hiv_status)[[2, 2]] + tabyl(mdf_prev_ind, hiv_status)[[2, 2]]
-tabyl(mdf_new_ind, age_cat)[[9, 2]] + tabyl(mdf_prev_ind, age_cat)[[9, 2]]
 
 # Appendix ------------------------------------------------
 
+# Table A1 - Associations between covariates and testing and resistance
+source("code/03_make_figures_and_tables/tab1_covs.R")
+
+save_new <- stargazer2(new_model_list)
+new_matrix <- read.table(textConnection(save_new), header = FALSE, sep = "\t")
+write.xlsx(new_matrix, paste0("output/figures_and_tables/tabA1_new", file_version_save, "_reg.xlsx"), colNames = FALSE, rowNames = FALSE)
+
+save_prev <- stargazer2(prev_model_list)
+prev_matrix <- read.table(textConnection(save_prev), header = FALSE, sep = "\t")
+write.xlsx(prev_matrix, paste0("output/figures_and_tables/tabA1_prev", file_version_save, "_reg.xlsx"), colNames = FALSE, rowNames = FALSE)
+
+# Missing count for Table 1 notes (there are no missing for health unit)
+# tabyl(mdf_new_ind, sex)[[3, 2]] + tabyl(mdf_prev_ind, sex)[[3, 2]]
+# tabyl(mdf_new_ind, hiv_status)[[2, 2]] + tabyl(mdf_prev_ind, hiv_status)[[2, 2]]
+# tabyl(mdf_new_ind, age_cat)[[9, 2]] + tabyl(mdf_prev_ind, age_cat)[[9, 2]]
