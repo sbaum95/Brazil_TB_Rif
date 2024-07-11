9c3746c453233cbeb552748b713fd7e16fd94565
sbaum95 <sbaum@g.harvard.edu>
2024-07-11 14:06:55 -0700

    Update in-line comments

--- code/01_data_processing/create_analytic_dataset.R
+++ code/01_data_processing/create_analytic_dataset.R
@@ -1,19 +1,18 @@
-# Author: Sarah Baum
-# Created: 2023-08-21
-# Updated: 2024-04-06
 
-# Description/Decisions: Takes sinan and prepare it for model analysis
+# Description/Decisions: Takes sinan and prepare it for model analysis. 
+# Filter to desired time period. Select only new and previously treated case types. Remove individuals misdiagnosed with TB. 
 
 create_analytic_dataset <- function(first_quarter, last_quarter, covariates, tratamento) {
   
   mdf <- sinan_tmp %>%
+    # Remove individuals misdiagnosed with TB (situa_ence == 06)
     filter(diag_qrt >= first_quarter & diag_qrt <= last_quarter & situa_ence != "06") %>%
     select(all_of(covariates))
   
-  ### Order the dates in ascending order
+  # Order the dates in ascending order
   sorted_dates <- sort(unique(mdf$diag_qrt))
   
-  ### Create a named vector to store the rank for each date
+  # Create a named vector to store the rank for each date
   time <- setNames(1:length(sorted_dates), format(sorted_dates, "%Y-%m-%d"))
   dates <- cbind(as.data.frame(sorted_dates), time)
   mdf <- left_join(mdf, dates, by = c("diag_qrt" = "sorted_dates"))
